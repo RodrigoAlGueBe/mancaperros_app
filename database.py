cf015ea90dd74c1332efbd63508f69c7d4c275c6
RodrigoAlGueBe <73110969+RodrigoAlGueBe@users.noreply.github.com>
2025-03-14 19:33:08 +0100

    database change

--- database.py
+++ database.py
@@ -2,13 +2,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-URL_DATABASE = ""
+# --------------------------- REMOTE CONNECTION ----------------------------
+DB_USER = 'erynfitadmin@erynfit'
+DB_PASSWORD = 'sgJW3a$@HfRmCx6'
+DB_HOST = 'erynfit.mysql.database.azure.com'
+DB_NAME = 'erynfit'
+DB_PORT = '3306'
+
+URL_DATABASE = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?ssl_ca=/etc/ssl/certs/ca-certificates.crt'
 
 engine = create_engine(URL_DATABASE)
 
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+# --------------------------------------------------------------------------
+
 
 # # --------------------------- LOCAL CONNECTION ----------------------------
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./mancaperros_app.db"
